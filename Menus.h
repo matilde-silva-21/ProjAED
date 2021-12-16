3ac681e92187057f877d99ed6c96420202f1a994
joao12520 <up202008867@edu.fe.up.pt>
2021-12-16 17:05:08 +0000

    menus

---
+++ Menus.h
@@ -0,0 +1,22 @@
+#ifndef AED2122_G53_MENUS_H
+#define AED2122_G53_MENUS_H
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Menus {
+
+};
+
+int airportMenu();
+
+int planesMenu();
+
+int flightsMenu();
+
+
+int publicTransMenu();
+
+#endif //AED2122_G53_MENUS_H
