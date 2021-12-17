6d28790c115e6277ff061421d9b9ff26d8c35640
Domingos Santos <domingosjmsantos@gmail.com>
2021-12-17 16:47:54 +0000

    Write

--- main.cpp
+++ main.cpp
@@ -32,7 +32,22 @@ std::vector<Airplane> ReadPlanes(){
         Airplane a(matricula, tipo, capacidade);
         avioes.push_back(a);
     }
+    f.close();
     return avioes;
+}
+
+void WritePlanes(Airplane &a){
+    fstream f("avioes.txt", ios::app);
+    string element;
+    element  = a.getMatricula() + ' ' + a.getTipo() + ' ' + to_string(a.getCapacidade());
+    f << element << endl;
+}
+
+void WritePassenger(Passenger &p){
+    fstream f("passageiros.txt", ios::app);
+    string element;
+    element  = p.getName() + ' ' + p.getEmail() + ' ' + p.getTipoID() + ' ' + p.getID() + ' ' + to_string(p.getPhone());
+    f << element << endl;
 }
 
 std::vector<Passenger> ReadPassageiros(){
@@ -52,6 +67,7 @@ std::vector<Passenger> ReadPassageiros(){
         Passenger p1 (nome, email, tipoID, phone, ID);
         passageiros.push_back(p1);
     }
+    f.close();
     return passageiros;
 }
 
@@ -71,6 +87,7 @@ std::vector<Employee> ReadEmployees(){
         Employee a1(name, employeeID);
         empregados.push_back(a1);
     }
+    f.close();
     return empregados;
 }
 
@@ -97,6 +114,7 @@ std::vector<Service> ReadServices(vector<Employee> empregados){
         Service c(tipo, t, it->getID());
         servicos.push_back(c);
     }
+    f.close();
     return servicos;
 }
 /*esta funcao assume que existirá um ficheiro para cada aviao , ficheiro com nome no formato ("Aviao"+matricula)
@@ -132,14 +150,13 @@ void readFlights(Airport& a1, string matricula) {
             Flight f1(numVoo, Time(day,month,year,starthour,startminute), Time(durationhour, durationminute));
             f1.setDestino(destino);
             f1.setOrigem(origem);
-
+            f.close();
             final.push_back(f1);
         }
         (*it).setPlanoVoo(final);
         a1.setAvioes(avioes);
     }
     else{cout<<"Plane does not exist!"<<endl; }
-
 }
 
 void readTickets(Airport& a1){
@@ -160,7 +177,6 @@ void readTickets(Airport& a1){
 
         a1.addTicket(t);
     }
-
 }
 int main() {
     Airport a1("Porto");
@@ -168,7 +184,15 @@ int main() {
     a1.setAvioes(avioes);
 
     readFlights(a1, "HH-33-HL");
-
+    string matricula = "44-gg-99";
+    string tipo = "A340";
+    int c = 678555434;
+    string nome = "Maria";
+    string IDtipo = "CC";
+    string mail = "gsjedgj@gmail.com";
+    int ID  = 344;
+    Passenger p(nome, mail, IDtipo, c, to_string(ID));
+    WritePassenger(p);
     return 0;
 }
 
